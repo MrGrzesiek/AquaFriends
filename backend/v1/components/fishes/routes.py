ce3b548890be6e4f895ecdc7d7fd5f978b8b8479
Mati-IR <mpabich4@gmail.com>
2024-05-30 11:30:53 +0200

    Fish species CRUD endpoints

--- backend/v1/components/fishes/routes.py
+++ backend/v1/components/fishes/routes.py
@@ -5,12 +5,32 @@ import sys
 from os import path
 sys.path.append(path.join(path.dirname(__file__), '...'))
 from models import User, FishSpecies
-from dependencies.auth.dependencies import get_admin_user
+from dependencies.auth import get_admin_user, get_current_user, admin_required, login_required
 
 router = APIRouter(prefix='/fishes')
 
+
+@admin_required
 @router.post('/species')
 def species(species: FishSpecies, user: User = Depends(get_admin_user)):
-    if not user or 'admin' not in user.scopes:
-        raise HTTPException(status_code=401, detail="User or Admin not in scope")
-    return "Fish species addin"
+    return {"message": "Not implemented yet", "code": "501"}
+
+
+@login_required
+@router.get('/species')
+def species(species_id: int, user: User = Depends(get_current_user)):
+    return {"message": "Not implemented yet", "code": "501"}
+
+
+@admin_required
+@router.put('/species')
+def species(species: FishSpecies, user: User = Depends(get_admin_user)):
+    return {"message": "Not implemented yet", "code": "501"}
+
+
+@admin_required
+@router.delete('/species')
+def species(species_id: int, user: User = Depends(get_admin_user)):
+    return {"message": "Not implemented yet", "code": "501"}
+
+
