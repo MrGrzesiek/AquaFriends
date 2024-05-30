8ac9a452753f06c7013450cf35ba282c5ade99c9
Mati-IR <mpabich4@gmail.com>
2024-05-30 15:22:00 +0200

    Fixed authentication bugs

--- backend/v1/dependencies/auth/wrappers.py
+++ backend/v1/dependencies/auth/wrappers.py
@@ -6,7 +6,7 @@ from .dependencies import manager
 def login_required(func):
     def wrapper(*args, **kwargs):
         if not manager.get_current_user():
-            raise HTTPException(status_code=401, detail="Not authenticated")
+            raise HTTPException(status_code=418, detail="Not authenticated XD :)")
         return func(*args, **kwargs)
 
     return wrapper
@@ -15,7 +15,7 @@ def admin_required(func):
     def wrapper(*args, **kwargs):
         user = manager.get_current_user()
         if not user or 'admin' not in user.scopes:
-            raise HTTPException(status_code=403, detail="Not enough permissions")
+            raise HTTPException(status_code=418, detail="Not enough permissions XD:)")
         return func(*args, **kwargs)
 
     return wrapper
