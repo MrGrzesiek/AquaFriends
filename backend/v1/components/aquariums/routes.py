124c0f775e43c77af900783fb436b931eaa894bc
Adam <adam@>
2024-06-11 21:21:46 +0200

    you can now add update and delete aquariums

--- backend/v1/components/aquariums/routes.py
+++ backend/v1/components/aquariums/routes.py
@@ -1,7 +1,7 @@
 from bson import ObjectId
 from fastapi import APIRouter, Depends, HTTPException
 
-from .utils import create_aquarium, get_all_aquariums, update_aquarium, delete_aquarium, get_aquarium_by_id
+from .utils import create_aquarium, get_all_aquariums, update_aquarium, delete_aquarium
 from dependencies.auth import get_admin_user, get_current_user, admin_required, login_required, \
     get_current_active_user
 from models import User, Aquarium
@@ -32,10 +32,8 @@ router = APIRouter(prefix='/Aquariums')
 @login_required
 @router.post('/new_aquarium')
 async def create_new_aquarium(aquarium: Aquarium, user: User = Depends(get_admin_user)):
-    aquarium_id = create_aquarium(aquarium)
-    if not aquarium_id:
-        raise HTTPException(status_code=500, detail="Failed to create Aquarium")
-    return {"Aquarium_id": str(aquarium_id)}
+    result = create_aquarium(aquarium)
+    return result
 
 
 @login_required
@@ -45,28 +43,15 @@ async def list_aquariums(user: User = Depends(get_current_user)):
     return aquariums
 
 
-@login_required
-@router.get('/{aquarium_id}')
-async def read_aquarium(aquarium_id: str, user: User = Depends(get_current_user)):
-    aquarium = get_aquarium_by_id(ObjectId(aquarium_id))
-    if not aquarium:
-        raise HTTPException(status_code=404, detail="Aquarium not found")
-    return aquarium
-
-
 @admin_required
 @router.put('/update/{aquarium_id}')
-async def update_existing_aquarium(aquarium: Aquarium, user: User = Depends(get_admin_user)):
-    success = update_aquarium(aquarium)
-    if not success:
-        raise HTTPException(status_code=500, detail="Failed to update Aquarium")
-    return {"message": "Aquarium updated successfully"}
+async def update_existing_aquarium(aquarium: Aquarium, aquarium_id, user: User = Depends(get_admin_user)):
+   result = update_aquarium(aquarium, aquarium_id)
+   return result
 
 
 @admin_required
 @router.delete('/delete/{aquarium_id}')
 async def delete_existing_aquarium(aquarium_id: str, user: User = Depends(get_admin_user)):
-    success = delete_aquarium(ObjectId(aquarium_id))
-    if not success:
-        raise HTTPException(status_code=500, detail="Failed to delete Aquarium")
-    return {"message": "Aquarium deleted successfully"}
+    result = delete_aquarium(aquarium_id)
+    return result
